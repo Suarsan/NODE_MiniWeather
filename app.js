b42e627b36aab2c5d524aa870abc3dfa1ef679e3
Javier Suarez <javiersuarsan@gmail.com>
2019-04-06 20:59:50 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const location = require('./location/location');
 const weather = require('./weather/weather');
 const argv = require('yargs').options({
-    direccion: {
+    address: {
         alias: 'd',
-        desc: 'Direccions de la ciudad para obtener el clima',
+        desc: 'City to obtain information',
         demand: true
     }
 }).argv;
 
-const getInfo = async(direccion) => {
+const getInfo = async(address) => {
 
     try {
-        const locationResult = await location.getCoordinates(argv.direccion);
+        const locationResult = await location.getCoordinates(address);
         const weatherResult = await weather.getWeather(locationResult.lat, locationResult.lon);
-        return { location: argv.direccion, temp: weatherResult.temp };
+        return { location: address, temp: weatherResult.temp };
     } catch (e) {
         console.dir(e);
     }
 
 }
 
-getInfo()
+getInfo(argv.address)
     .then(res => {
-        console.dir('                                                 '),
-            console.dir('    -----------------------------------------    '),
-            console.dir('    |                                       |    '),
-            console.dir(`    |   Temperature in ${res.location} is ${res.temp}°   |    `),
-            console.dir('    |                                       |    '),
-            console.dir('    -----------------------------------------    ')
-        console.dir('                                                 ')
+        console.dir('                                                           '),
+            console.dir('       --------------------------------------------           '),
+            console.dir('                                                              '),
+            console.dir(`             Temperature in ${res.location} is ${res.temp}°              `),
+            console.dir('                                                              '),
+            console.dir('       --------------------------------------------           ')
+        console.dir('                                                              ')
     })
     .catch(err => { throw new Error(err) });
